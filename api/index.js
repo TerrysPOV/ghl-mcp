a60bdbcbd433373d0e8f22105a44a80907c3d6af
TerrysPOV <terry.yodaiken@poview.ai>
2025-07-25 08:02:09 +0100

    updated index.js to resolve to the correct tools folder

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const SERVER_INFO = {
 };
 
 // Dynamic tool loading
-const toolsDirectory = path.join(__dirname, "tools");
+const toolsDirectory = path.join(__dirname, "..", "src", "tools");
 const loadedTools = [];
 
 fs.readdirSync(toolsDirectory).forEach(file => {
